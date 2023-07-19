18329d107db633a7dd4b4e009d32fe36803683d6
hashtanveer <tanveerhash7@gmail.com>
2023-07-19 14:38:05 +0530

    Disease output and load model integrated

--- AI/urls.py
+++ AI/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import DiseaseDetectionCreationView, DetectionInformation, ListDetectionsForUser, ListDetectionModels
 
+app_name = 'AI'
+
 urlpatterns = [
-    path('create', DiseaseDetectionCreationView.as_view()),
+    path('create', DiseaseDetectionCreationView.as_view(), name='create'),
     path('info', DetectionInformation.as_view()),
     path('list', ListDetectionsForUser.as_view()),
-    path('models', ListDetectionModels.as_view())
+    path('models', ListDetectionModels.as_view(), name='models')
 ]
--- website/templates/static/js/detectionScript.js
+++ website/templates/static/js/detectionScript.js
@@ -1,13 +1,37 @@
 const dragArea = document.querySelector('.drag-area');
 const dragText = document.querySelector('.header');
+const cropsDropdown = document.querySelector('#crops-dropdown');
+const diseaseHolder = document.querySelector('#diseaseHolder');
 
 let button = document.querySelector('.button');
 let input = document.querySelector('input');
 
 let file;
 
+cropsDropdown.addEventListener("DOMContentLoaded", function() {
+    console.log("Logging")
+  });
 
-
+function loadModels(){
+    console.log("Loading models");
+    const access = localStorage.getItem('access_token');
+    fetch(modelsUrl,  {
+    headers: {Authorization: 'Bearer ' + access},
+     })
+     .then(response => response.json())
+     .then(data =>{
+        const { models } = data;
+        for(var crop in models){
+            var opt = document.createElement('option');
+            opt.value = models[crop];
+            opt.innerHTML = models[crop];
+            opt.style = "font-size: small;"
+            cropsDropdown.appendChild(opt);
+        }
+     })
+     .catch(error => { console.error(error) });
+}
+loadModels();
 button.onclick = () => {
     input.click();
 };
@@ -22,20 +46,17 @@ dragArea.addEventListener('dragover', (event) => {
     event.preventDefault();
     dragText.textContent = 'Release to Upload';
     dragArea.classList.add('active');
-    // console.log('File is inside the drag area');
 });
 
 dragArea.addEventListener('dragleave', () => {
     dragText.textContent = 'Drag & Drop';
     dragArea.classList.remove('active');
-    // console.log('File left the drag area');
 });
 
 dragArea.addEventListener('drop', (event) => {
     event.preventDefault();
 
-    file = event.dataTransfer.files[0]; // how many files to upload
-    // console.log(file);
+    file = event.dataTransfer.files[0]; 
     displayFile();
 });
 
@@ -64,7 +85,6 @@ let fileType = file.type;
 
 const createImage = async (event) => {
     event.preventDefault()
-    const imageEndpoint = 'http://127.0.0.1/detection/create'
     let image = file
     let formData = new FormData()
 
@@ -72,9 +92,19 @@ const createImage = async (event) => {
     formData.append('plant_type', 'Potato')
     const access = localStorage.getItem('access_token')
 
-    let newImage = await fetch(imageEndpoint,  {
+    let newImage = await fetch(createUrl,  {
        headers: {Authorization: 'Bearer ' + access},
       method: 'POST',
       body: formData
-    }).then(response => response.json()).catch(error => { console.error(error) })
+    })
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data);
+        const { _complete } = data;
+        if(_complete){
+            const { disease } = data;
+            diseaseHolder.innerHTML = disease;
+        }
+    })
+    .catch(error => { console.error(error) })
 }
